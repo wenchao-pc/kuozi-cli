aeff643835d8890f129269a3a4cf0d11ec92f102
wenchao <kuozi@kuo-zi.local>
2017-05-15 19:49:27 +0800

    add vue-desktop-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
+//js工具库 _
+import _ from "lodash";
 // css初始化
 import "normalize.css";
 
