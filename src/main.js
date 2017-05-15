d15ec2cec7b4d2dda74bb7716b8071adda22347d
wenchao <kuozi@kuo-zi.local>
2017-05-15 18:52:03 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import _ from "lodash";
 import Vue from "vue";
+//js工具库 _
+import _ from "lodash";
+// css初始化
 import "normalize.css";
+//css动画库
+import "animate.css";
 import "./styles/style.less";
 import router from './routers/router';
 import store from './store/store';
@@ -11,6 +15,14 @@ import "./mixin/mixin";
 import "./filtres/filter";
 import App from "./App";
 
+//手势操作
+import VueTouch = 'vue-touch';
+Vue.use(VueTouch, { name: 'v-touch' });
+
+import FastClick from "fastclick";
+
+// 去掉ios上300毫秒延迟
+FastClick.attach(document.body);
 
 Vue.config.devtools = true;
 new Vue({
